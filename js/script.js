ed9ec365e00c4bfed2219d0127c89c17f7f5aca3
Сауле Ойчиева <oichievas>
2021-05-25 12:36:20 +0600

    fixed just intut file

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $("#slider").owlCarousel(
     $(".owl-carousel").owlCarousel({
       autoplay: true,
+      autoplayTimeout: 2000,
       loop: true,
       margin: 10,
       responsiveClass: true,
